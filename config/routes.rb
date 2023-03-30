dfaf0796c4d4a554305a7f814bfb85fda7371a84
Brandon Baker <brandonbaker@Brandons-MacBook-Pro.local>
2023-03-30 19:35:12 -0400

    initial OAuth configuration less environment variable installation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, 
+             controllers: {                                 
+               sessions:           'users/sessions',                                                                             
+               omniauth_callbacks: 'users/omniauth_callbacks'                                      
+             }
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get 'welcome/index'
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'welcome#index'
 end
