b7824a7ac0071dee3246999552bc887d46d2f175
Brandon Baker <brandonbaker@Brandons-MacBook-Pro.local>
2023-04-03 22:58:55 -0400

    changes user ids to be uuids like all other models

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_30_221501) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_04_025249) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
