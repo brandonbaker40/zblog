75afe4be9781fe67d18fe2eb0598bb6a9f927cfb
Brandon Baker <brandonbaker@Brandons-MacBook-Pro.local>
2023-04-27 14:04:01 -0400

    adds faker for testing load on turbo

---
+++ db/migrate/20230427175918_add_birthdate_to_patients.rb
@@ -0,0 +1,5 @@
+class AddBirthdateToPatients < ActiveRecord::Migration[7.0]
+  def change
+    add_column :patients, :birthdate, :date
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+require 'faker'
+
+500.times do |i|
+  sex = nil
+  rand(1..2) == 1 ? sex = :male : sex = :female
+  Patient.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    sex: sex,
+    birthdate: Faker::Date.between(from: '1923-01-01', to: '1975-12-31')
+  )
+end
