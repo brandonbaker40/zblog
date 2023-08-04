d87ee5306f04d286c01d45db8ded282245bf9f4f
Brandon Baker <brandonbaker40@icloud.com>
2023-08-04 07:34:06 -0400

    tidies up profile model by removing unused resources

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,5 @@
 class Profile < ApplicationRecord
   after_create :import_payroll_data_from_paychex
-  # after_update :sync_payroll_data_from_paychex
 
   has_one :user_profile
 
@@ -17,20 +16,16 @@ class Profile < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
 
-
   # validates :ssn, presence: true
   # validates_format_of :ssn, with: /\A((?!219-09-9999|078-05-1120)(?!666|000|9\d{2})\d{3}-(?!00)\d{2}-(?!0{4})\d{4})|((?!219 09 9999|078 05 1120)(?!666|000|9\d{2})\d{3} (?!00)\d{2} (?!0{4})\d{4})|((?!219099999|078051120)(?!666|000|9\d{2})\d{3}(?!00)\d{2}(?!0{4})\d{4})\z/
   # encrypts :ssn
+
 private
 
 def import_payroll_data_from_paychex
-  # TestService::BigGuy.new(self).illuminate
   # Authenticate on Paychex API
   token = PaychexApiService::Authenticate.new.call
-
-
   PaychexApiService::BuildWorkerProfile.new(self, token).call
-  # Paycheck API Service Get worker
 end
 
 end
